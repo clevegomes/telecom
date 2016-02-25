d696d5016752865de2f360cd69d3f1ff9e465697
Cleve <gomescleve@gmail.com>
2016-02-25 17:52:30 +0400

    first commit

---
+++ v1/index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Author : Cleve Gomes
+ */
+require_once "Telecom.php";
+
+
+//http://localhost/api/v1/phoneno/list                       All phone numbers
+//http://localhost/api/v1/phoneno/customer/john              All phone numbers of customer john
+//http://localhost/api/v1/phoneno/activate/0507856712        Activate a number.
+
+
+try
+{
+
+	$telecomObjAPI = new Telecom($_REQUEST["request"]);
+	echo $telecomObjAPI->processAPI();
+}
+catch (Exception $e)
+{
+	echo json_encode(Array("error"=>$e->getMessage()));
+}
+
+
+
+
+
+
